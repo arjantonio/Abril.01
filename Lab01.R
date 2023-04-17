bba8a4b0921da6acf6929b8deafec614788273f7
melissayauri <yaurimelissa@gmail.ccom>
2023-04-17 10:15:30 -0500

    Renombrando columnas

--- Lab01.R
+++ Lab01.R
@@ -3,5 +3,9 @@ library(stringr)
 library(dplyr)
 library(tidyr)
 
+# Importando el archivo
 info_data <- read_table("D:/epa-http.csv", col_names = FALSE)
+# Renonbrando los encabezados de las columnas
+colnames(info_data) <- c("Directions", "Timestamp", "Method", "Resource", "Protocol", "Response Code", "Size")
+
 View(info_data)
