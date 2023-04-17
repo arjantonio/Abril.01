e2f59c326928eef2167b39226a5561a1a6542874
arjantonio <arjantonio@hotmail.com>
2023-04-17 10:38:10 -0500

    Actualizacion de columnas

--- Lab01.R
+++ Lab01.R
@@ -9,3 +9,8 @@ info_data <- read_table("D:/epa-http.csv", col_names = FALSE)
 colnames(info_data) <- c("Directions", "Timestamp", "Method", "Resource", "Protocol", "Response Code", "Size")
 
 View(info_data)
+info_data$Siza <- as.numeric(info_data$Size)
+info_data$Method2 <- str_sub(info_data$Method, 2)
+info_data$Method2 <- as.factor(info_data$Method2)
+info_data$Fecha01 <- strptime(info_data$Timestamp, format = "[%d:%H:%M:%S]")
+info_data$Fecha02 <- str_sub(info_data$Fecha01, -8)
